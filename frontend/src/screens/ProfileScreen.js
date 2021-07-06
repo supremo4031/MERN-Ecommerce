ead6b33f4812d80cd7619efa8b848ab168087bf8
supremo4031 <66715995+polygonsolution@users.noreply.github.com>
2021-07-06 16:52:58 +0530

    profile name bug fixed

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -6,6 +6,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
 import { myOrderList } from '../actions/orderActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const ProfileScreen = ({ location, history }) => {
 	const [name, setName] = useState('')
@@ -28,7 +29,8 @@ const ProfileScreen = ({ location, history }) => {
 		if (!userInfo) {
 			history.push('/login')
 		} else {
-			if (!user.name) {
+			if (!user || !user.name || success) {
+				dispatch({ type: USER_UPDATE_PROFILE_RESET })
 				dispatch(getUserDetails('profile'))
 				dispatch(myOrderList())
 			} else {
@@ -36,7 +38,7 @@ const ProfileScreen = ({ location, history }) => {
 				setEmail(user.email)
 			}
 		}
-	}, [dispatch, userInfo, history, user])
+	}, [dispatch, userInfo, history, user, success])
 
 	const submitHandler = (e) => {
 		e.preventDefault()
