1cf22941578634eadcbb9503b0cc6a9c8fa4b577
supremo4031 <66715995+polygonsolution@users.noreply.github.com>
2021-08-16 23:13:53 +0530

    inventory controller added

---
+++ backend/controllers/inventoryController.js
@@ -0,0 +1,42 @@
+import asyncHandler from 'express-async-handler'
+import Category from '../models/categoryModel.js'
+import Inventory from '../models/inventoryModel.js'
+import ErrorResponse from '../utils/ErrorResponse.js'
+
+// @desc    Fetch inventory by seller id
+// @route   GET /api/inventory/seller/:id
+// @access  Public
+export const getInventoriesBySellerId = asyncHandler(async (req, res) => {
+	
+    const inventory = Inventory.find({ seller: req.params.id })
+
+	res.status(200).json({
+		inventory,
+	})
+})
+
+// @desc    Fetch inventory by product id
+// @route   GET /api/inventory/product/:id
+// @access  Public
+export const getInventoriesByProductId = asyncHandler(async (req, res) => {
+	
+    const inventory = Inventory.find({ product: req.params.id })
+
+	res.status(200).json({
+		inventory,
+	})
+})
+
+// @desc    Fetch inventory by product id
+// @route   GET /api/inventory/product/:id
+// @access  Public
+export const getInventoriesByProductAndSellerId = asyncHandler(async (req, res) => {
+	
+    const inventory = Inventory.findOne({ product: req.params.pid, seller: req.params.sid })
+
+	res.status(200).json({
+		inventory,
+	})
+})
+
+
